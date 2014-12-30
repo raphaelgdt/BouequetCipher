2ffb07938d345cffe53d6cc75b51427ba6ccf642
raphaelgdt <raphael.gdt@gmail.com>
2014-12-30 16:04:26 +1100

    webGL page for Amy Ireland's Cipher
    Cipher is a webGL exercise presenting each phoneme-shape of Amy
    Ireland’s Bouequet 3D printed artwork.
    The current script consists in a patchwork of different three.js
    examples

---
+++ shapeList.js
@@ -0,0 +1,24 @@
+var shapes = [
+    {"shapePath":"models/i_sound.js" , "shapePhon":"i"},
+    {"shapePath":"models/m_sound.js", "shapePhon":"m"},
+    {"shapePath":"models/a_sound.js", "shapePhon":"æ"},
+    {"shapePath":"models/k_sound.js", "shapePhon":"k"},
+    {"shapePath":"models/n_sound.js", "shapePhon":"n"},
+    {"shapePath":"models/p_sound.js", "shapePhon":"p"},
+    {"shapePath":"models/s_sound.js", "shapePhon":"s"},
+    {"shapePath":"models/v_sound.js", "shapePhon":"v"},
+    {"shapePath":"models/l_sound.js", "shapePhon":"l"},
+    {"shapePath":"models/b_sound.js", "shapePhon":"b"},
+    {"shapePath":"models/h_sound.js", "shapePhon":"h"},
+    {"shapePath":"models/o_sound.js", "shapePhon":"o℧"},
+    {"shapePath":"models/e_sound.js", "shapePhon":"e"},
+    {"shapePath":"models/d_sound.js", "shapePhon":"d"},
+    {"shapePath":"models/AA_sound.js", "shapePhon":"a"},
+    {"shapePath":"models/EE_sound.js", "shapePhon":"ei"},
+    {"shapePath":"models/w_sound.js", "shapePhon":"w"},
+    {"shapePath":"models/u_sound.js", "shapePhon":"u:"},
+    {"shapePath":"models/y_sound.js", "shapePhon":"e"},
+    {"shapePath":"models/t_sound.js", "shapePhon":"t"},
+    {"shapePath":"models/UU_sound.js", "shapePhon":"y"}
+              ];
+
---
+++ shapeListAdvanced.js
@@ -0,0 +1,24 @@
+var shapes = [
+    {"shapePath":"models/i_sound.js" , "shapePhon":"i","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/m_sound.js", "shapePhon":"m","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/a_sound.js", "shapePhon":"æ","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/k_sound.js", "shapePhon":"k","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/n_sound.js", "shapePhon":"n","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/p_sound.js", "shapePhon":"p","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/s_sound.js", "shapePhon":"s","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/v_sound.js", "shapePhon":"v","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/l_sound.js", "shapePhon":"l","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/b_sound.js", "shapePhon":"b","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/h_sound.js", "shapePhon":"h","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/o_sound.js", "shapePhon":"o","sScale1":1,"sFont":"Arial","shapePhon2":"℧","sScale2":.7,"sFont2":"Arial"},
+    {"shapePath":"models/e_sound.js", "shapePhon":"e","sScale1":1,"sFont":"Arial","shapePhon2":"I","sScale2":.75,"sFont2":"Georgia"},
+    {"shapePath":"models/d_sound.js", "shapePhon":"d","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/AA_sound.js", "shapePhon":"a","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/EE_sound.js", "shapePhon":"l","sScale1":1,"sFont":"Arial","shapePhon2":"ə","sScale2":1,"sFont2":"Arial"},
+    {"shapePath":"models/w_sound.js", "shapePhon":"w","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/u_sound.js", "shapePhon":"u:","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/y_sound.js", "shapePhon":"e","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/t_sound.js", "shapePhon":"t","sScale1":1,"sFont":"Arial"},
+    {"shapePath":"models/UU_sound.js", "shapePhon":"y","sScale1":1,"sFont":"Arial"}
+              ];
+
