69d44e145622d283de5f054e78cc9af6e6d04841
raphaelgdt <raphael.gdt@gmail.com>
2015-12-10 08:16:06 +1100

    adding bump
    models with uvs and shader with bump map

---
+++ shapeListAdvanced4.js
@@ -0,0 +1,9 @@
+var shapes = [
+    {"shapePath":"models/amyten3.js"},
+
+    {"shapePath":"models/bataille3.js"},              
+
+
+    {"shapePath":"models/bogost3.js"}            
+              ];
+
